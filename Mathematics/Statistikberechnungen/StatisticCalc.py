a839fab289bfdc7600059122334a34cfbabcdaae
DGrothe-PhD <dag_fs@t-online.de>
2024-05-15 19:33:18 +0200

    finished layout

--- Mathematics/Statistikberechnungen/StatisticCalc.py
+++ Mathematics/Statistikberechnungen/StatisticCalc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from flask import Flask, render_template
 from flask import request
+from flask_table import Table, Col
 
 import ast
 import statistics as stats
@@ -8,7 +9,16 @@ import numpy as np
 from py_markdown_table.markdown_table import markdown_table
 
 # initialization
+class QuantileTable(Table):
+    Name = Col('Name')
+    Average = Col('Mittelwert')
+    Minimum = Col('Min')
+    Quartile1 = Col('Q1')
+    Median = Col('Median')
+    Quartile3 = Col('Q3')
+    Maximum = Col('Max')
 
+    
 
 # Run app
 app = Flask(__name__)
@@ -22,16 +32,12 @@ def evalQuart():
     result = ''
     if request.method == 'POST':
         raw_numbers = request.form['numbers']
-        numbers = = list(ast.literal_eval(raw_numbers.strip("()[]{}")))
+        numbers = list(ast.literal_eval(raw_numbers.strip("()[]{}")))
         name = request.form['name']
         result = EvaluateQuantiles(numbers, name)
-    elif request.method = 'GET':
+    elif request.method == 'GET':
         pass
     return render_template('index.html', tableofresults = result)
-
-
-if __name__ == '__main__':
-    app.run()
 
 # Calculus
 #example_data = [43.07, 39.23, 44.76, 42.15, 39.98, 40.5, 46.08, 38.62]
@@ -54,18 +60,24 @@ def EvaluateQuantiles(data_list, name = ""):
     results = [
         {
             "Name": name,
+            "Average" : Avg,
             "Minimum" : sorted_data[0],
-            "Q1" : Q1,
+            "Quartile1" : Q1,
             "Median" : Median,
-            "Q3": Q3,
+            "Quartile3": Q3,
             "Maximum" : sorted_data[-1],
-            "Average" : Avg
+
         }
     ]
     
-    tableofresults = markdown_table(results).get_markdown()
+    tableofresults = QuantileTable(results)
+    #tableofresults = markdown_table(results).get_markdown()
     return tableofresults
 
 # Example
 
 #EvaluateQuantiles(example_data, "Beispiel")
+
+
+if __name__ == '__main__':
+    app.run()
